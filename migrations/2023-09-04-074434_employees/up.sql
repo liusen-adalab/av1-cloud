a68576f38afc7b695ea1e8215f5b4916bb2bcb50
liuzsen <yjghdsss@gmail.com>
2023-09-04 16:02:01 +0800

    补充 employees 数据库表文件

---
+++ migrations/2023-09-04-074434_employees/up.sql
@@ -0,0 +1,23 @@
+-- Your SQL goes here
+CREATE TABLE employees(
+    id BIGINT NOT NULL,
+
+    name VARCHAR(20) NOT NULL,
+    mobile_number VARCHAR(16) UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+
+    -- 使用 argon2 处理后，长度固定为 97 字节，但有可能变更
+    password CHAR(97) NOT NULL,
+
+    last_login TIMESTAMP NOT NULL DEFAULT NOW(),
+    
+    invited_by BIGINT NOT NULL,
+    role SMALLINT NOT NULL DEFAULT 0,
+
+    create_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    PRIMARY KEY (id)
+);
+
+SELECT diesel_manage_updated_at('employees');
--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,21 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    employees (id) {
+        id -> Int8,
+        name -> Varchar,
+        mobile_number -> Nullable<Varchar>,
+        email -> Varchar,
+        password -> Bpchar,
+        last_login -> Timestamp,
+        invited_by -> Int8,
+        role -> Int2,
+        create_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int8,
         name -> Varchar,
@@ -15,17 +30,7 @@ diesel::table! {
     }
 }
 
-diesel::table! {
-    employees (id) {
-        id -> Int8,
-        name -> Varchar,
-        mobile_number -> Nullable<Varchar>,
-        email -> Varchar,
-        password -> Bpchar,
-        last_login -> Timestamp,
-        create_at -> Timestamp,
-        updated_at -> Timestamp,
-        invited_by -> Int8,
-        role -> Int2,
-    }
-}
+diesel::allow_tables_to_appear_in_same_query!(
+    employees,
+    users,
+);
